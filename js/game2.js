4ce8c7af2f79951907f7dd144aa957857a40d7b9
Alex Kayser <akctba@gmail.com>
2019-10-04 09:36:53 -0700

    Prime 1

--- js/game2.js
+++ js/game2.js
@@ -3,6 +3,9 @@ function Prime()
     var i,flag=0,number;
     number = Number(document.getElementById("q2-input").value);
     name = document.getElementById("q1-input").value;
+    if(number < 2) 
+        flag = 1;
+    else
     for(i=2; i <= number/2; i++)
     {
         if(number%i == 0)
